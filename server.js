667f100c4ce2f1d65c9ee7678e2d73a47f7d5fe4
pkimany254 <pkimany254@gmail.com>
2026-08-31 21:36:25 +0300

    Update popularity filter threshold to 0.5

--- server.js
+++ server.js
@@ -673,7 +673,7 @@ async function newReleases() {
       movies
         .filter(
           x =>
-            (x.popularity || 0) >= 0 &&
+            (x.popularity || 0) >= 0.5 &&
             (x.vote_count || 0) >= 0 &&
             Boolean(x.poster_path)
         )
@@ -692,7 +692,7 @@ async function newReleases() {
       tv
         .filter(
           x =>
-            (x.popularity || 0) >= 0 &&
+            (x.popularity || 0) >= 0.5 &&
             (x.vote_count || 0) >= 0 &&
             Boolean(x.poster_path)
         )
