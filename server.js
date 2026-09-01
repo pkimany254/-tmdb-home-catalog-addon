0194fd27ada8e675635b6575e3e61a945dad1031
pkimany254 <pkimany254@gmail.com>
2026-09-01 19:54:29 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1723,7 +1723,7 @@ async function latestOnDigital() {
         include_adult:
           "false"
       },
-      100
+      150
     );
 
   /* -------------------------------------------------------
