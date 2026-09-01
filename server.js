1ec83ed22cfc83ecfa80756e20a8df0a0515de0f
pkimany254 <pkimany254@gmail.com>
2026-09-01 19:13:06 +0300

    Adjust minimum popularity and increase movie slice limit

--- server.js
+++ server.js
@@ -1704,7 +1704,7 @@ async function latestOnDigital() {
     day();
 
   const voteCountMinimum = 0;
-  const popularityMinimum = 0;
+  const popularityMinimum = 0.5;
 
   let movies =
     await tmdbPages(
@@ -1871,7 +1871,7 @@ async function latestOnDigital() {
     digitalMovies
   )
     .map(movieMeta)
-    .slice(0, 100);
+    .slice(0, 200);
 }
 
 /* =========================================================
